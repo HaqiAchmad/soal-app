453fdab1bd2ef9572a7a362929564663f27b7e96
Achmad Baihaqi <hakiahmad756@gmail.com>
2019-04-18 17:13:45 +0700

    menambahkan file include/loading.h

---
+++ include/loading.h
@@ -0,0 +1,16 @@
+#ifndef LOADING_H
+#define LOADING_H
+
+
+class loading
+{
+    public:
+        loading();
+        virtual ~loading();
+
+    protected:
+
+    private:
+};
+
+#endif // LOADING_H
