bf231c6c99fc7ff55120f9bb474106b4f7358af2
Achmad Baihaqi <hakiahmad756@gmail.com>
2019-05-03 12:04:33 +0700

    menambahkan file include/tes soal.h

---
+++ include/tes soal.h
@@ -0,0 +1,16 @@
+#ifndef TES SOAL_H
+#define TES SOAL_H
+
+
+class tes soal
+{
+    public:
+        tes soal();
+        virtual ~tes soal();
+
+    protected:
+
+    private:
+};
+
+#endif // TES SOAL_H
