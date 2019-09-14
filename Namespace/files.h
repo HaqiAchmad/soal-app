bf25c3d960a4f9babaaee0835e79f9a36f7040ba
Achmad Baihaqi <hakiahmad756@gmail.com>
2019-09-14 17:59:01 +0700

    mengupdate file Namespace/files.h

--- Namespace/files.h
+++ Namespace/files.h
@@ -10,10 +10,9 @@
 
 namespace files{
     using namespace std;
-    using namespace decor;
-//    using namespace funct;
     using namespace getValues;
-//    using namespace getValues;
+    using namespace decor;
+
 
 void addAccount(){
 
@@ -36,7 +35,7 @@ void addAccount(){
         getName(Nama); // untuk mendapatkan nilai dari variabel nama
 
             if (Nama.length() <= 2){
-                errName = "Nama terlalu pendek!!";
+                errName = "Nama terlalu pendek!!"; // jika input nama kurang dari 2 character maka akan goto change:
                 goto change;
             }
 
@@ -65,7 +64,7 @@ void addAccount(){
         cout << errGmail <<endl;
         cout << "Masukan gmail anda : ";
             cin >> Gmail;
-        getGmail(Gmail);
+        getGmail(Gmail); // mendapatkan value dari variabel Gmail
 
     /// mengecek yang di masukan user gmail atau bukan
     int lengthStr = Gmail.length();
@@ -88,7 +87,7 @@ void addAccount(){
 
 
 
-
+        // mengecek apakah yang dimasuakan user gmail atau bukan
         int GM,tru; bool Fail = false;
         for (int r = 0; r < 10; r++){
             tru = Checking[r];
@@ -106,7 +105,7 @@ void addAccount(){
         else {
             Login = false;
         }
-    }
+    }// end while
 
     string Pass,KonPass;
     bool isPassword = true;
@@ -115,17 +114,17 @@ void addAccount(){
     /// user memasukan password
     while(isPassword == true){
 
+        // menampilkan gmail yang dimasukan user ke console
        ThrowPassword: system("cls");
        textcolor(15); cout <<"\nMasukan Gmail anda  : "; textcolor(8); cout << Gmail; textcolor(12); cout << pesan <<endl;
 
        textcolor(15); cout <<"Masukan Password    : "; textcolor(8);
-          //  cin.ignore(numeric_limits<streamsize>::max(),'\n');
             getline(cin,Pass);
 
-         if (Pass.length() <= 5){
+         if (Pass.length() <= 5){  // jika password kurang <= 6 maka di throw keatas
             pesan = "  //Password harus >= 6 character";
             system("cls");
-            goto ThrowPassword; /// jika password kurang <= 6 maka di throw keatas
+            goto ThrowPassword;
          }else
          {
             textcolor(15); cout <<"Konfirmasi Password : "; textcolor(8);
@@ -150,7 +149,7 @@ void addAccount(){
 
 
 void loginUserAccount(){
-    string acco, pass, fnName, fnAcc, fnPass, buffer, errMsg;
+    string inputUser, pass, fnName, fnAcc, fnPass, buffer, errMsg;
     ifstream files;
     files.open("C:\\MinGW\\codeblock\\Soal APP\\Database\\user\\UserAccount.txt");
     bool isFind = false, isBlock;
@@ -160,10 +159,11 @@ void loginUserAccount(){
    system("cls"); textcolor(15);
    textcolor(15); cout << errMsg <<endl;
     cout <<"\nMasukan Gmail Anda : "; textcolor(8);
-      cin >> acco;
+      cin >> inputUser;
 
    while (!files.eof()){
-     getline(files, buffer);
+
+    getline(files, buffer);
         fnName = buffer;
     getline(files, buffer);
         fnAcc = buffer;
@@ -171,16 +171,16 @@ void loginUserAccount(){
         fnPass = buffer;
     getName(fnName);
 
-        if (fnAcc == acco){
+        if (fnAcc == inputUser){ // jika akun di temukan maka bool isFind akan true
             isFind = true;
             break;
         }
    }
+    getGmail(inputUser); // mendapatkan value dari variabel inputUser
 
-    getGmail(acco);
-//    getGmailacco);
     if (isFind == true){
 
+        // action jika bool isFind bernilai true
         system("cls");
         textcolor(12); cout << fnAcc <<" ditemukan!!!"<<endl;
         cin.ignore(numeric_limits<streamsize>::max(),'\n');
@@ -200,7 +200,7 @@ void loginUserAccount(){
             chance-=1;
         }
     }else if (isFind == false){
-        errMsg = acco+" belum terdaftar\n Silahkan cek akun anda kembali \n Jika anda belum mempunyai akun silahkan ketik out pada console";
+        errMsg = inputUser+" belum terdaftar\n Silahkan cek akun anda kembali \n Jika anda belum mempunyai akun silahkan ketik out pada console";
         //goto notFound;
     }
 
@@ -211,44 +211,95 @@ void loginUserAccount(){
 
     }
 
-
 }
 
 void getLoginType(){
 
-    int choose;
+    time_t t = time(NULL);
+    tm* timePtr = localtime(&t);
+    char choose; string ucapan; 
+    int Jam = timePtr->tm_hour; // <-- mendapatkan waktu di komputer
 
-   //TypingTxt("Welcome", 12, 300, 35, 3);
-   textcolor(11);
-  notFound:
-  kampret(0, 9); cout <<"\n    >> 1. Daftar "<<endl;
-  kampret(0, 11); cout <<"    >> 2. Masuk "<<endl;
-  cout << "Input code [1/2] ";
-        cin>> choose;
+    // string Motivasi = "cara termudah jadi pandai adalah belajar dari hal terbodoh yang pernah kamu lakukan.";
+    // string Motivasi = "Dengan begitu kemampuanmu akan lebih berkembang lagi. Dan pastinya, kamu tak akan jatuh ke lubang yang sama dua kali. Kata kata motivasi belajar ini cocok banget nih buat yang lagi menyesali kesalahan di masa lalu.";
+    string Motivasi = "Padahal sebenarnya, pendidikan yang kamu sebut menjengkelkan ini berkah lho. Karena berkatnya mungkin kamu bisa jadi orang hebat, bisa meraih cita cita yang diidamkan dan bisa meraih kehidupan yang berkecukupan di masa depan.";
+    string spasi = "-             ";
 
-        if (choose == 1){
-            addAccount();
+    // mendeteksi waktu apakah pagi, siang, sore, petang atau malam
+    if (Jam <= 9 && Jam >= 5){ // pagi dimulai jam 5 sampai 9
+        ucapan = "Selamat Pagi";
+    }else if (Jam <= 14 && Jam >= 10){ // siang dimulai jam 10 sampai 14
+        ucapan = "Selamat Siang";
+    }else if (Jam <= 17 && Jam >= 15){ // sore dimulai jam 15 sampai 17
+        ucapan = "Selamat Sore";
+    }else if (Jam <= 19 && Jam >= 18){ // petang dimulai jam 18 sampai 19
+        ucapan = "Selamat Petang";
+    }else if (Jam <= 23 && Jam >= 20){ // malam dimulai jam 20 sampai 23
+        ucapan = "Selamat Malam";
+    }else if (Jam <= 4 && Jam >= 0){ // malam dimulai jam 0 sampai 4
+        ucapan = "Selamat Malam";
+    }
 
-        }else if (choose == 2){
-            loginUserAccount();
+    notFound: // goto : jika input dari user tidak ditemukan
+
+    cout << endl <<endl;
+    decor::loading(1,80,9,1); // menampilkan efek garis di gotoxy(0,3);
+
+    decor::gotoxy(0,22);
+    decor::loading(1,80,9,1); // menampilkan efek garis di gotoxy(0,22);
+
+
+    // menambahkan spasi di tengah" kata (agar tampilan lebih cantik saat di tampilkan di console)
+    if (Motivasi.length() >= 66){ 
+    Motivasi.insert(66, spasi); 
+    }
+    if (Motivasi.length() >= 147){
+    Motivasi.insert(147, spasi);
+    }
+    if (Motivasi.length() >= 226){
+    Motivasi.insert(226, spasi);
+    }
+    if (Motivasi.length() >= 306){
+    Motivasi.insert(306, spasi);
+    }
+    if (Motivasi.length() >= 385){
+        Motivasi.insert(385, spasi);
+    }
+
+    // menampilkan ucapan Selamat Pagi/Siang/Sore/Petang/Malam di gotoxy (33,4) dengan color light green
+    decor::gotoxy(33,4); decor::textcolor(10); 
+    cout << ucapan <<endl;
+
+    // menampilkan value variabel Motivasi di gotoxy(1,15);
+    decor::gotoxy(3,15); decor::textcolor(15);
+    cout <<"Note : "; decor::textcolor(8); cout << Motivasi <<endl; 
+
+    // menapilkan pilihan menu Created account di gotoxy(3,9);
+    decor::gotoxy(3, 9); decor::textcolor(10);
+    cout <<">>"; decor::textcolor(15); cout <<" 1. ";decor::textcolor(12); cout <<"Created account!"<<endl;
+    // menampilkan pilihan menu Have a account already? di gotoxy(3,10);
+    decor::gotoxy(3, 10); decor::textcolor(10);
+    cout <<">>"; decor::textcolor(15); cout <<" 2. ";decor::textcolor(12); cout <<"Have a account already?"<<endl;
+    // menampilkan Input Code[1/2]? : di gotoxy (3,14) dengan color light yellow
+    decor::gotoxy(3, 12); decor::textcolor(14);
+    cout <<"Input code[1/2]? "; 
+    decor::textcolor(15); cout <<"-> "; decor::gotoxy(24,12); cout <<" <-" <<endl; // membuat efek anak panah
+    // mendapatkan input dari user
+    decor::textcolor(11); decor::gotoxy(23, 12);
+    cin >> choose; 
+
+        if (choose == '1'){
+            addAccount(); // menambahkan akun
+        }else if (choose == '2'){
+            loginUserAccount(); // Log in dengan akun yang sudah ada
         }else {
             system("cls");
-            goto notFound;
+            goto notFound; // jika pilihan tidak ditemukan maka akan dilempar ke atas lagi
         }
 
+
+
 }
-
-
- void appClose(){
-
-     Sound(2); system("cls");textcolor(15);
-         randC("5.2.4",33,10,300,21);
-         kampret(25,19);cout <<endl;
-            textcolor(12);
-        system("pause");
-        textcolor(0);
-
- }
 
 void readDataUser(){
     ifstream dataUser;
@@ -281,33 +332,6 @@ void readUserAccount(){
     }
     system("pause");
       textcolor(9);
-}
-
-void getOption(){
-     string choose; loop:
-     kampret(29,11);textcolor(10);
-     cout <<"Start Program y/n ? =  "; textcolor(9);
-     cin >> choose;
-
-     // ok == sudah di buat
-     system("cls");
-     if (choose == "y" || choose == "Y"){
-        getLoginType();
-
-     }else if (choose == "n" || choose == "N" || choose == "out"){
-          appClose(); // ok
-
-     }else if (choose == "read.data" || choose == "##"){
-        readDataUser(); // ok
-        system("cls");
-        goto loop;
-
-     }else if (choose == "read.acc" || choose == "#!"){
-        readUserAccount(); //ok
-        system("cls");
-        goto loop;
-     }
-
 }
 
  void writeDataUser(){
