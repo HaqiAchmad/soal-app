43e608fe1f2b4d667a5fd8e005780c663a12660e
Achmad Baihaqi <hakiahmad756@gmail.com>
2019-10-04 22:35:25 +0700

    mengupdate file Namespace/decor.h

--- Namespace/decor.h
+++ Namespace/decor.h
@@ -12,11 +12,11 @@
 namespace decor{
 
     using namespace std;
-    void gotoxy(int x, int y){
-COORD coord;
-coord.X = x;
-coord.Y = y;
-SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+void gotoxy(int x, int y){
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 void textcolor(int ForgC) {
      WORD wColor;
@@ -30,87 +30,126 @@ void textcolor(int ForgC) {
      }
      return;
  }
-void Sound(int a){
-    ///   ADD -lwinmm  in linker
-    switch(a){
-case 1 :
-   PlaySound(TEXT("Database\\Sound\\Login.wav"),NULL,SND_SYNC);
-    break;
-case 2:
-    PlaySound(TEXT("Database\\Sound\\Logout.wav"),NULL,SND_SYNC);
-    break;
-case 3:
-    PlaySound(TEXT("Database\\Sound\\Success.wav"),NULL,SND_SYNC);
-    break;
-case 4:
-    PlaySound(TEXT("Database\\Sound\\notify.wav"),NULL,SND_SYNC);
-    break;
-case 5:
-    PlaySound(TEXT("Database\\Sound\\Next.wav"),NULL,SND_SYNC);
-    break;
-case 6:
-    PlaySound(TEXT("Database\\Sound\\Denied.wav"),NULL,SND_SYNC);
-    break;
-case 7:
-    PlaySound(TEXT("Database\\Sound\\Exit.wav"),NULL,SND_SYNC);
-    break;
-case 8:
-    PlaySound(TEXT("Database\\Sound\\Error.wav"),NULL,SND_SYNC);
-    break;
-    }
+
+void colorPosition(int horizontal, int vertikal, int color=15){
+    gotoxy(horizontal, vertikal);
+    textcolor(color);
+
+}
+void sound(int code){ ///   ADD -lwinmm  in linker
+
+  string activated = "disable";
+
+  if (activated == "enable"){
+    //     switch(code){
+    // case 1 :
+    //     PlaySound(TEXT("Database\\Sound\\Login.wav"),NULL,SND_SYNC);
+    //     break;
+    // case 2:
+    //     PlaySound(TEXT("Database\\Sound\\Logout.wav"),NULL,SND_SYNC);
+    //     break;
+    // case 3:
+    //     PlaySound(TEXT("Database\\Sound\\Success.wav"),NULL,SND_SYNC);
+    //     break;
+    // case 4:
+    //     PlaySound(TEXT("Database\\Sound\\notify.wav"),NULL,SND_SYNC);
+    //     break;
+    // case 5:
+    //     PlaySound(TEXT("Database\\Sound\\Next.wav"),NULL,SND_SYNC);
+    //     break;
+    // case 6:
+    //     PlaySound(TEXT("Database\\Sound\\Denied.wav"),NULL,SND_SYNC);
+    //     break;
+    // case 7:
+    //     PlaySound(TEXT("Database\\Sound\\Exit.wav"),NULL,SND_SYNC);
+    //     break;
+    // case 8:
+    //     PlaySound(TEXT("Database\\Sound\\Error.wav"),NULL,SND_SYNC);
+    //    break;
+    //   }
+
+  }else if (activated == "disable"){
+     //pass
+  }
+
+
 }
 
-void randC (string randStr,int horizl, int vertkl,int lama,int akhir){
+void randC (string randStr,int horizl, int vertkl,int lama, int akhir){
 
-     time_t t = time(NULL);
+    time_t t = time(NULL);
     tm* timePtr = localtime(&t);
+    int rendem1, rendem2;
 
-    for (int i = 0; i <= akhir; i++){
+        for (int i = 0; i <= akhir; i++){
 
-  int toValue = timePtr->tm_sec;
-  int RandClr = 1 + (rand() & 14);
-  int VarClr = 1 + (rand() & 14);
-
-     textcolor(RandClr+VarClr);
-      gotoxy(horizl,vertkl);  cout << randStr <<endl;
+            rendem1 = timePtr->tm_sec;
+            textcolor (1 + rand() & timePtr->tm_sec);
+            gotoxy(horizl,vertkl);
+            cout << randStr <<endl;
         Sleep(lama);
     textcolor(15);
   }
 }
 void writeText (string Text,int clrTxt, int sleepTxt, int horzntl, int vrtkl){
-    int length = Text.length();
-    char Teksch[length];
-strcpy(Teksch, Text.c_str());
+
     textcolor(clrTxt);
-        gotoxy(horzntl,vrtkl);
-    for (int i = 0; i < length; i++){
-        cout << Teksch[i];
-            Sleep(sleepTxt);
-    }
+    gotoxy(horzntl,vrtkl);
+
+        for (int i = 0; i < Text.length(); i++){
+            cout << Text[i];
+                Sleep(sleepTxt);
+        }
     textcolor(15);
 }
 void teksJalan_kanan (string NameStr,int beginLoc, int endLoc, int Nl_vertikal,int colorTxt,int speedJLN){
-int Mulai = beginLoc - NameStr.length(); // Mengambil posisi paling kanan
+
+  int Mulai = beginLoc - NameStr.length(); // Mengambil posisi paling kanan
+
+  textcolor(colorTxt);
     for (int i = Mulai; i >= endLoc; i--){
-             textcolor(colorTxt);
-        gotoxy(i,Nl_vertikal); cout << NameStr <<" "<<endl;
-    Sleep(speedJLN);
+      gotoxy(i, Nl_vertikal);
+        cout << NameStr <<" "<<endl;
+        Sleep(speedJLN);
     }
 
-textcolor(15);
+  textcolor(15);
 }
 void teksJalan_kiri (string NameStr,int beginLoc,int endLoc, int Nl_vertikal,int colorTxt,int speedJLN){
-    int Mulai = beginLoc;
-        for (int j = Mulai; j < endLoc; j++){
-               // system("cls");
-            textcolor(colorTxt);
-            gotoxy(j,Nl_vertikal);
-                cout <<" "<< NameStr <<endl;
-                Sleep(speedJLN);
-        }
+
+  int Mulai = beginLoc;
+
+  textcolor(colorTxt);
+    for (int j = Mulai; j < endLoc; j++){
+        gotoxy(j, Nl_vertikal);
+            cout <<" "<< NameStr <<endl;
+            Sleep(speedJLN);
+    }
+
+  textcolor(15);
+}
+
+
+void showRules(int horizontal, int vertikal, array<string, 3> &rules, string errMessage){
+
+    gotoxy(horizontal, vertikal);
+    textcolor(9);
+      cout <<"_______________________________________________________________________________"<<endl <<endl;
+
+      for (int i = 0; i < 3; i++){
+        textcolor(8);
+          cout << " - ";
         textcolor(15);
+          cout << rules[i];
+      }
+
+      textcolor(9);
+      cout <<"\n_______________________________________________________________________________"<<endl;
+      textcolor(15);
+      cout <<"\n Message : "; teksJalan_kanan(errMessage, 79, 11, vertikal+7, 12, 20);
 
 }
+
 void loading (int aw,int akr,int color, int Delay){
     for (;aw <= akr;aw++){
         textcolor(color);
@@ -118,6 +157,27 @@ void loading (int aw,int akr,int color, int Delay){
             Sleep(Delay);
     }
     textcolor(15);
+}
+void loadingGotoxy(int horizontal, int vertikal, int color, int delay, char pil='+'){
+
+    if (pil == '+'){ // jika pil bernilai '+' maka efek loading dimulai dari kiri
+        gotoxy(horizontal, vertikal);
+        for (int i = 0; i <= 79; i++){
+            textcolor(color);
+        cout <<'=';
+        Sleep(delay);
+        }
+    }else if (pil == '-'){
+        for (int i = horizontal; i >= 0; i--){ // jika pil bernilai '-' maka efek loading dimulai dari kanan
+            gotoxy(i, vertikal);
+            textcolor(color);
+            cout <<'=';
+            Sleep(delay);
+        }
+    }
+
+
+
 }
 
 
